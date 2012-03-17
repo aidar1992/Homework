21128d34a7f6fa971f14d24cf0581740f20b3bbf
Aidar <aidar1992@bk.ru>
2012-03-17 21:23:40 +0300

    first

---
+++ MinMaxVariables.java
@@ -0,0 +1,31 @@
+
+public class MinMaxVariables {
+
+
+	
+
+	public static void main(String[] args) {
+		System.out.print("Integer ");
+		System.out.print(Integer.MAX_VALUE+" ");
+		System.out.println(Integer.MIN_VALUE);
+		System.out.print("Byte ");
+		System.out.print(Byte.MAX_VALUE+" ");
+		System.out.println(Byte.MIN_VALUE);
+		System.out.print("Double ");
+		System.out.print(Double.MAX_VALUE +" ");
+		System.out.println(Double.MIN_VALUE);
+		System.out.print("Float ");
+		System.out.print(Float.MAX_VALUE+" ");
+		System.out.println(Float.MIN_VALUE);
+		System.out.print("Long");
+		System.out.print(Long.MAX_VALUE +" ");
+		System.out.println(Long.MIN_VALUE);
+		System.out.print("Short ");
+		System.out.print(Short.MAX_VALUE+" ");
+		System.out.println(Short.MIN_VALUE);
+		
+		
+		
+	}
+
+}
